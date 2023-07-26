bea12f6de7131d5b891bc1f1532951396377fa09
umut <umut_baykan@hotmail.com>
2023-07-26 13:22:39 +0100

    basic JS codes

---
+++ code.js
@@ -0,0 +1,107 @@
+// Some JS bits
+
+////// Fetch
+
+const imageClientCall = (userChoices) => {
+  fetch("/images", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: userChoices,
+  })
+  .then((response) => {
+    if (!response.ok) {
+      const error = new Error(`HTTP Error: Status ${response.status}`);
+      error.status = response.status;
+      throw error;
+    }
+    return response.json();
+  })
+    .then((data) => {
+      setImgUrl(data.imgUrl);
+      updateStorageAndHooks("imageHistory", data["imgUrl"]);
+      setSDLoaded(true);
+    });
+};
+
+////// Iterations
+for (let i = 0; i < 10; i++) {
+  // do something with your loop
+}
+
+while (i<10) {
+  // do something
+}
+
+////// Objects
+
+// Access the key value pairs of an object
+Object.entries(object).map(([key, value]) => {
+  // do something with key
+  // do something with value
+ })
+
+// Access the keys of an object
+Object.keys(object).map((key) => {
+  // do something with key
+})
+
+for (property in object) {
+  // do something with key
+}
+
+// Access the values of an object
+Object.values(object).reduce(
+  (sum, value) => sum + value, 0
+)
+
+/////// Arrays
+
+// Creation
+Array.from({length: 5}, () => {return "hello"}) // -> [ 'hello', 'hello', 'hello', 'hello', 'hello' ]
+
+// Grid creation with cell components
+{Array.from({ length: gameState.boards[index].size }, (_, rowIndex) => (
+  <div className="row" key={rowIndex}>
+    {Array.from(
+      { length: gameState.boards[index].size },
+      (_, colIndex) => (
+        <Cell
+          key={colIndex}
+          coordinates={[rowIndex, colIndex]}
+          display={
+            (parsedBoard[JSON.stringify([rowIndex, colIndex])] || {})
+              .symbol
+          }
+          type={
+            (parsedBoard[JSON.stringify([rowIndex, colIndex])] || {})
+              .class
+          }
+          action={action}
+        />
+      )
+    )}
+  </div>
+))}
+
+// Iterate over - does not modify elements at all
+arr.forEach((element) => {
+  return element *= 2
+})
+
+arr.forEach((element, index) => {
+  // do something
+  // all iterations of arr can take an index in as a second parameter
+})
+
+// Mapping
+arr.map((element) => {
+  return element *= 2
+}) // <-- change the element within, does not modify original array
+
+// Filtering
+arr.filter((element) => {
+  if (element === "something") {return true}
+  // this does not modify the original array
+})
